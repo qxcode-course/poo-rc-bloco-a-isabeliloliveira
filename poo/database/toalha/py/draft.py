3fe094c35a935127be366d881c8744f01fb28d29
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-09-17 14:57:52 +0000

    2 hrs atividade

---
+++ poo/database/toalha/py/draft.py
@@ -0,0 +1,22 @@
+class Towel: #classe
+    def __init__(self, color:str, size:str): #construtor
+        self.color: str = color #atributo
+        self.size: str = size
+        self.wetness: int = 0
+
+def __str__(self) -> int
+
+#referencias
+print("qual a cor e o tamanho da sua toalha?")
+color= input("")
+tamanho= input("")
+towel: Towel = Towel(color, tamanho) #objeto
+print(f"Sua toalha é {towel.color} e seu tamanho é {towel.size}")
+# minha.color = "blue"
+# minha.size = "P"
+
+# esposa = Towel()
+# esposa.color = "red"
+# esposa.size = "G"
+
+# gaiato=esposa
