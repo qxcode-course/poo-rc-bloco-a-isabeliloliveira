05d52b48f267a34b985b8fa314cb2ce877df5e90
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-10-05 17:30:45 +0000

    summit

---
+++ poo/database/carro/py/draft.py
@@ -0,0 +1,8 @@
+class Carro:
+    def __init__(self, pass: int):
+        self.pass: int = pass
+        self.passMax: int = 2
+        self.gasMax: int = 100
+        self.km: int = 0
+    
+    def 
