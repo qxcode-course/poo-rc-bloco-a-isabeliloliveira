aad4a7a93ff7ed367f497f1a849bc2ff1027864a
isabeliloliveira <isabeli.oliveira@alu.ufc.br>
2025-10-05 23:52:30 +0000

    summit

--- poo/database/carro/py/draft.py
+++ poo/database/carro/py/draft.py
@@ -3,6 +3,46 @@ class Carro:
         self.pass: int = pass
         self.passMax: int = 2
         self.gasMax: int = 100
+        self.gas: int = 0
         self.km: int = 0
     
-    def 
+    def __str__(self) -> str:
+        return f"pass: {self.pass}, gas: {self.gas}, km {self.km}"
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+def main():
+    carro = Carro(0)
+    while True:
+        line: str = input()
+        print(f"$" + line)
+        args: list[str] = line.split(" ")
+
+        if args[0] == "show":
+            print(carro)
+
+        elif args[0] == "enter":
+            carro.enter()
+
+        elif args[0] == "leave":
+            carro.leave()
+        elif args[0] == "end":
+            break
+
+        elif args[0] == "fuel":
+            increment = int(args[1])
+            carro.fuel(increment)
+
+        elif args[0] == "drive":
+            increment = int(args[1])
+            carro.drive(increment)
+            
+main()
